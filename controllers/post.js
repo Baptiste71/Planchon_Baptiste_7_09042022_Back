a065efa89b18852bc5abdbcbad37946083d2a7b8
Baptiste71 <planchonbaptiste@gmail.com>
2022-05-26 18:50:42 +0200

    mise en place moderation posts

--- controllers/post.js
+++ controllers/post.js
@@ -55,36 +55,38 @@ exports.addElement = async (req, res) => {
 
 exports.deletePost = async (req, res) => {
   let userId = req.auth.userId;
+  let admin = Boolean;
+  if (req.auth.email === process.env.ADMIN) {
+    admin = true;
+  } else {
+    admin = false;
+  }
   let postAuthor = "";
 
   try {
-    let postdelete = await Post.findOne({
+    let userPost = await Post.findOne({
       where: {
         id: req.body.id,
       },
-    })
-      .then((post) => {
-        postAuthor = post.userId;
+    }).then((post) => {
+      postAuthor = post.userId;
+      let author = Boolean;
+      if (userId === postAuthor) {
+        author = true;
+      } else {
+        author = false;
+      }
 
-        if (userId != postAuthor) {
-          return res.status(401).json({ message: "requête non autorisée !" });
-        }
-      })
-      .catch((error) => res.status(500).json({ error }));
-
-    postdelete = await Post.findOne({
-      where: {
-        id: req.body.id,
-      },
+      if (admin || author) {
+        return post;
+      } else {
+        return res.status(401).json({ message: "requête non autorisée !" });
+      }
     });
-
-    const filename = postdelete.image;
-    fs.unlink(`image/${filename}`, () => {
-      postdelete.destroy();
-      return res.json({ message: "Post deleted!" });
-    });
+    await userPost.destroy();
+    return res.status(200).json({ message: "Post deleted" });
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ error: "Something went wrong!" });
+    return res.status(500);
   }
 };
